3622df602883247be226b3c52e7b65b10359b61d
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-21 10:25:42 -0600

    added auth mw and config/constants and jest.config, added users-model and service, and filled out auth-router for register and login functions.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,82 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
+const constants = require("../config/constants.js");
+
+router.post("/register", (req, res) => {
   // implement registration
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash password
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save user to db
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide username and password. (The password shoud be alphanumeric)",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = signToken(user);
+
+          res.status(200).json({
+            message: "Welcome to the show",
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "Please provide username and password. (The password shoud be alphanumeric)",
+    });
+  }
 });
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const secret = constants.jwtSecret;
+
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+const constants = require("../config/constants.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // add code here to verify users are logged in
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, constants.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        // token not valid or was modified
+        res.status(401).json({ YOU: "SHALLL NOT PASSSSHHHH" });
+      } else {
+        // token is good and we have access to the information inside
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
